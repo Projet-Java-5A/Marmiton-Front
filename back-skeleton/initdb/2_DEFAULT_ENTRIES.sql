04dc070f54ac6f0c12f0aeeaacebc2caf1caf70a
Maxime <mpakula@takima.fr>
2024-10-14 12:11:18 +0200

    fix: Removed hardcoded ids in sql scripts

--- back-skeleton/initdb/2_DEFAULT_ENTRIES.sql
+++ back-skeleton/initdb/2_DEFAULT_ENTRIES.sql
@@ -1,25 +1,34 @@
-INSERT INTO majors (id, name, description) VALUES (1, 'Informatique', 'Ouaiiis du code partout');
-INSERT INTO majors (id, name, description) VALUES (2, 'Construction', 'Beaucoup de béton et des poutres');
-INSERT INTO majors (id, name, description) VALUES (3, 'Aéronautique', 'Vive le vent');
-INSERT INTO majors (id, name, description) VALUES (4, 'Data', 'Trop cool plein de données à ordonner');
-INSERT INTO majors (id, name, description) VALUES (5, 'Energie & Environnement', 'On est full green');
-INSERT INTO majors (id, name, description) VALUES (6, 'Management', 'Des managers de qualité');
-INSERT INTO majors (id, name, description) VALUES (7, 'Santé', 'On connait tous les os et tous les muscles du corps humain');
-INSERT INTO majors (id, name, description) VALUES (8, 'Architecture durable', 'Objectif 0 carbone');
-INSERT INTO majors (id, name, description) VALUES (9, 'Design Industriel Durable', 'On resistera à la fin du pétrole');
+Do $$
 
-INSERT INTO students (id, first_name, last_name, birthdate, major_id) VALUES (1, 'Paul', 'Harrohide', '2002-06-15', 1);
-INSERT INTO students (id, first_name, last_name, birthdate, major_id) VALUES (2, 'Jean', 'Bonbeur', '2001-08-21', 1);
-INSERT INTO students (id, first_name, last_name, birthdate, major_id) VALUES (3, 'Alain', 'Térieur', '2000-01-11', 1);
+    DECLARE _STUDENT_1 int := NEXTVAL('students_id_seq');
+    DECLARE _MAJOR_1 int := NEXTVAL('majors_id_seq');
+    DECLARE _COURSE_1 int := NEXTVAL('courses_id_seq');
 
-INSERT INTO courses (id, name, hours) VALUES (1, 'Spanish', 30);
-INSERT INTO courses (id, name, hours) VALUES (2, 'German', 30);
-INSERT INTO courses (id, name, hours) VALUES (3, 'Internet of Things', 30);
-INSERT INTO courses (id, name, hours) VALUES (4, 'Thermodynamic', 30);
-INSERT INTO courses (id, name, hours) VALUES (5, 'Anatomy', 30);
-INSERT INTO courses (id, name, hours) VALUES (6, 'Maths', 30);
-INSERT INTO courses (id, name, hours) VALUES (7, 'Java', 30);
-INSERT INTO courses (id, name, hours) VALUES (8, 'Lean Management', 30);
-INSERT INTO student_course (id, student_id, course_id) VALUES (1, 1, 7);
+    BEGIN
 
+    INSERT INTO majors (id, name, description) VALUES (_MAJOR_1, 'Informatique', 'Ouaiiis du code partout');
+    INSERT INTO majors (name, description) VALUES ('Construction', 'Beaucoup de béton et des poutres');
+    INSERT INTO majors (name, description) VALUES ('Aéronautique', 'Vive le vent');
+    INSERT INTO majors (name, description) VALUES ('Data', 'Trop cool plein de données à ordonner');
+    INSERT INTO majors (name, description) VALUES ('Energie & Environnement', 'On est full green');
+    INSERT INTO majors (name, description) VALUES ('Management', 'Des managers de qualité');
+    INSERT INTO majors (name, description) VALUES ('Santé', 'On connait tous les os et tous les muscles du corps humain');
+    INSERT INTO majors (name, description) VALUES ('Architecture durable', 'Objectif 0 carbone');
+    INSERT INTO majors (name, description) VALUES ('Design Industriel Durable', 'On resistera à la fin du pétrole');
+
+    INSERT INTO students (id, first_name, last_name, birthdate, major_id) VALUES (_STUDENT_1, 'Paul', 'Harrohide', '2002-06-15', _MAJOR_1);
+    INSERT INTO students (first_name, last_name, birthdate, major_id) VALUES ('Jean', 'Bonbeur', '2001-08-21',_MAJOR_1);
+    INSERT INTO students (first_name, last_name, birthdate, major_id) VALUES ('Alain', 'Térieur', '2000-01-11', _MAJOR_1);
+
+    INSERT INTO courses (id, name, hours) VALUES (_COURSE_1, 'Java', 30);
+    INSERT INTO courses (name, hours) VALUES ('German', 30);
+    INSERT INTO courses (name, hours) VALUES ('Internet of Things', 30);
+    INSERT INTO courses (name, hours) VALUES ('Thermodynamic', 30);
+    INSERT INTO courses (name, hours) VALUES ('Anatomy', 30);
+    INSERT INTO courses (name, hours) VALUES ('Maths', 30);
+    INSERT INTO courses (name, hours) VALUES ('Spanish', 30);
+    INSERT INTO courses (name, hours) VALUES ('Lean Management', 30);
+    INSERT INTO student_course (student_id, course_id) VALUES (_STUDENT_1, _COURSE_1);
+
+    END $$;
 
