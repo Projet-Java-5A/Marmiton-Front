113af33214ee44cf29dee4dcc8a823116f5dd45e
lsegouin <lou-anne.segouin@epfedu.fr>
2025-10-13 11:42:11 +0200

    feat: homepage non finie mais arthur me met la pression jsp pourquoi

--- front-skeleton/src/app/home/home.component.ts
+++ front-skeleton/src/app/home/home.component.ts
@@ -1,12 +1,33 @@
 import { Component, OnInit } from "@angular/core"
 
+interface Recette {
+  nom: string;
+  imageUrl: string;
+}
 @Component({
   selector: "home",
   templateUrl: "./home.component.html",
   styleUrls: ["./home.component.scss"],
 })
-export class HomeComponent implements OnInit {
-  constructor() {}
 
-  ngOnInit(): void {}
+export class HomeComponent {
+  // Tableau avec les recettes à afficher
+  favoris: Recette[] = [
+    {
+      nom: 'Tarte aux framboises',
+      imageUrl: 'https://images.pexels.com/photos/1327821/pexels-photo-1327821.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+    },
+    {
+      nom: 'Gratin dauphinois',
+      imageUrl: 'https://images.pexels.com/photos/15085149/pexels-photo-15085149/free-photo-of-gratin-de-pommes-de-terre-dans-un-plat-en-ceramique.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+    },
+    {
+      nom: 'Tarte au thon',
+      imageUrl: 'https://images.pexels.com/photos/8992926/pexels-photo-8992926.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+    },
+    {
+      nom: 'Gâteau au chocolat',
+      imageUrl: 'https://images.pexels.com/photos/4109998/pexels-photo-4109998.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+    }
+  ];
 }
