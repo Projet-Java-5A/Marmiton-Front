fd9f9f46c9b9ceb48f06a9b7e2ce6ae72853303a
Arthur <arthur.abdelli@epfedu.fr>
2025-10-26 12:15:37 +0100

    Add : Le bouton admin n'est visible que lorsque l'on est admin

--- front-skeleton/src/app/footer/footer.component.ts
+++ front-skeleton/src/app/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-footer',
@@ -7,6 +8,10 @@ import { Component } from '@angular/core';
 })
 export class FooterComponent {
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
+
+  isAdmin(): boolean {
+    return this.authService.isAdmin();
+  }
 
 }
