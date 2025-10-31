1139927b7d497510b685b49c82278030691c7413
Arthur <arthur.abdelli@epfedu.fr>
2025-10-31 11:33:21 +0100

    Fix : bouton retour à la page home

--- front-skeleton/src/app/navbar/navbar.component.ts
+++ front-skeleton/src/app/navbar/navbar.component.ts
@@ -1,21 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { Observable } from 'rxjs';
 import { UserDto } from '../models/user.dto';
-import { Router } from '@angular/router';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'navbar', // Assurez-vous que le sélecteur est correct
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
   // Exposition de l'utilisateur courant en tant qu'observable pour le template
   currentUser$: Observable<UserDto | null>;
+  isHomePage: boolean = true;
 
   constructor(private authService: AuthService, private router: Router) {
     // On récupère le flux de données de l'utilisateur depuis le service
     this.currentUser$ = this.authService.currentUser$;
+  }
+
+  ngOnInit(): void {
+    this.router.events.pipe(
+      filter((event): event is NavigationEnd => event instanceof NavigationEnd)
+    ).subscribe((event: NavigationEnd) => {
+      this.isHomePage = event.urlAfterRedirects === '/';
+    });
   }
 
   // Méthode pour appeler la déconnexion
